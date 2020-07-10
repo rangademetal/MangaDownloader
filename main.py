2457c47f1d8c2f0b2f7e2c0c5f53d0fe5c96e1d9
rangademetal <60630480+rangademetal@users.noreply.github.com>
2020-07-10 07:27:49 -0700

    README.md added

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from Downloader import mangadex
 # downloader.randomStory()
 # downloader.getImage()
 
-downloader = mangadex.Mangadex(path = '/usr/local/bin/chromedriver')
+downloader = mangadex.Mangadex(path ='C:\Program Files (x86)\chromedriver.exe')
 downloader.getPageMangadex()
 downloader.login()
 downloader.search(manga='Devil May Cry 5 -Visions of V-')
-downloader.download(path='/Users/andreistroe/Pictures')
+downloader.download(path='C:/Users/stroe/Pictures')
