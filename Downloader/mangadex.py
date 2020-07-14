27963b38388cbdb2c271a32392084993f0262401
rangademetal <Sad1996.09.05>
2020-07-14 12:08:03 +0300

    mangadex fix

--- Downloader/mangadex.py
+++ Downloader/mangadex.py
@@ -14,7 +14,7 @@ class Mangadex:
         self.driver.maximize_window()
 
     # serching manga
-    def login(self):
+    def login(self, username, password):
         login = self.driver.find_element_by_xpath('//*[@id="navbarSupportedContent"]/ul[2]/li[2]/a')
         login.click()
         login = self.driver.find_element_by_xpath('//*[@id="navbarSupportedContent"]/ul[2]/li[2]/div/a[1]/span')
@@ -22,10 +22,10 @@ class Mangadex:
         # login access
         login = self.driver.find_element_by_xpath('//*[@id="login_username"]')
         login.click()
-        login.send_keys('guest123qwe')
+        login.send_keys(username)
         login = self.driver.find_element_by_xpath('//*[@id="login_password"]')
         login.click()
-        login.send_keys('guest123qwe')
+        login.send_keys(password)
         login = self.driver.find_element_by_xpath('//*[@id="login_button"]/span')
         login.click()
 
@@ -46,7 +46,7 @@ class Mangadex:
 
 
         # Getting manga
-    def DownloadSelectedChapter(self, link, path):
+    def downloadSelectedChapter(self, link, path):
 
         self.driver.get(link)
         time.sleep(2)
@@ -60,34 +60,3 @@ class Mangadex:
             nextpage = self.driver.find_element_by_xpath('//*[@id="content"]/div[1]/div/div[2]/div[8]/a[2]/span')
             nextpage.click()
             print(item)
-
-    def langselector(self):
-        time.sleep(1)
-        select = self.driver.find_element_by_xpath('//*[@id="quick_search_input"]')
-        select.click()
-        select.send_keys(' ')
-        select = self.driver.find_element_by_xpath('//*[@id="quick_search_button"]/span')
-        select.click()
-
-        #
-        select = self.driver.find_element_by_xpath('//*[@id="lang_id"]')
-        print(select.text)
-        select = self.driver.find_element_by_xpath('//*[@id="lang_id"]/option[3]')
-        select.click()
-        select = self.driver.find_element_by_xpath('/html/body')
-        select.click()
-
-    def download(self, path):
-        time.sleep(4)
-        title = random.randint(11111111, 99999999)
-        count = 0
-        os.chdir(path)
-        os.mkdir(str(title))
-        while True:
-            time.sleep(2)
-            download = self.driver.find_element_by_xpath('//*[@id="content"]/div[2]/div[2]/div/img').get_attribute('src')
-            print(download)
-            urllib.request.urlretrieve(download, str(title) + '/' + str(count) + '.jpg')
-            nextpage = self.driver.find_element_by_xpath('//*[@id="content"]/div[1]/div/div[2]/div[8]/a[2]/span')
-            nextpage.click()
-            count +=1
