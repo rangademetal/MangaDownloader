c305cfc831234e6ea0b85a58b46350505ce7b543
rangademetal <60630480+rangademetal@users.noreply.github.com>
2020-07-14 23:30:50 -0700

    Update exemple1.py

--- exemple/exemple1.py
+++ exemple/exemple1.py
@@ -4,8 +4,8 @@ downloader = mangadex.Mangadex(path='F:\selenium\chromedriver.exe')
 downloader.login(username='guest123qwe', password='guest123qwe')
 path = downloader.createFolder('F:\manga')
 
-# Exemple 1
 # Download the single chapter
+# Use the method numberPage with the link of chapter then download
 number = downloader.numberPages(link='https://mangadex.org/chapter/559372/1')
 downloader.downloadSelectedChapter(items=number, path=path)
 downloader.quit()
