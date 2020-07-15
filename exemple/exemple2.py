00bb231aa8e7b764fac1c539ca0471d0aa16c3bc
rangademetal <60630480+rangademetal@users.noreply.github.com>
2020-07-14 23:24:56 -0700

    Update exemple2.py

--- exemple/exemple2.py
+++ exemple/exemple2.py
@@ -2,20 +2,19 @@ from Downloader import mangadex
 
 # Create an object
 downloader = mangadex.Mangadex(path='F:\selenium\chromedriver.exe')
-#
-downloader.login(username='guest123qwe', password='guest123qwe')
+
 # create the dir and save it in path variable
+downloader.login(username='guest123qwe', password='guest123qwe')
 path = downloader.createFolder('F:\manga')
 
-# Creating the NULL array to save all pages of manga chapters
+# Creating the NULL array to save all pages of the manga chapters
 # create a new array 
-arr = []
 # send the browser on the link
+# the method numberPagesChapter return the page of the chapters
+arr = []
 downloader.getLink('https://mangadex.org/chapter/559372/1')
-# this method return the page of chapters
 n = downloader.numberPagesChapter(items=arr)
 
 # after the pages are knews, the download is started
 downloader.getLink('https://mangadex.org/chapter/559372/1')
-# After knows the all pages, the download has begin
 downloader.downloadAllPages(arr=n, path=path)
